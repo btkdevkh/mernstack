b61ee1472bbfdef70b31195f6f67ef62893b8199
btkdevkhdelli5 <amcjkbtk91@outlook.com>
2022-02-19 17:33:39 +0100

    Added frintend UI

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,68 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import { FaSignInAlt } from "react-icons/fa"
+
+export default function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please login</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input 
+              type="email" 
+              className="form-comtrol" 
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input 
+              type="password" 
+              className="form-comtrol" 
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  )
+}
